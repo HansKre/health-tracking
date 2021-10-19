378b5f13e431069766ece45d3a8c7639b3c100e6
hans <hans.krebs@me.com>
2021-10-19 09:49:58 +0200

    Customize webpack config with craco.

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const { override, addWebpackAlias } = require('customize-cra');
+
+const path = require('path');
+
+module.exports = override(
+  addWebpackAlias({
+    '@animations': path.resolve(__dirname, './src/components/animations'),
+    '@components': path.resolve(__dirname, './src/components'),
+    '@config': path.resolve(__dirname, './src'),
+    '@styles': path.resolve(__dirname, './src/styles'),
+  })
+);
